d1f6a1a113748c93929c0cc94caeb6cb38346951
Alex Novak <alexwnovak@gmail.com>
2019-01-03 11:33:47 -0500

    +fluent assertions

--- src/sigil.Tests/UnitTest1.cs
+++ src/sigil.Tests/UnitTest1.cs
@@ -1,4 +1,5 @@
 using System;
+using FluentAssertions;
 using Xunit;
 
 namespace sigil.Tests
@@ -6,9 +7,15 @@ namespace sigil.Tests
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void PassingTest()
         {
+            (1+2).Should().Be(3);
+        }
 
+        [Fact]
+        public void FailingTest()
+        {
+            (1+1).Should().Be(5);
         }
     }
 }
